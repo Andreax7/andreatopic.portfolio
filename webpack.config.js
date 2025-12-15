6ee8bac5720af5246aff70069565844613b3be73
Andreax7 <andreaatx7@gmail.com>
2025-12-15 16:08:58 +0100

    newfixurls

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
- output: {
-  path: path.resolve(__dirname, 'docs'),
-  filename: 'bundle.js',
-  publicPath: '/andreatopic.portfolio/', // Ensures relative paths
- },
+  output: {
+    path: path.resolve(__dirname, "docs"), // Build folder
+    filename: "bundle.js",
+    publicPath: "/andreatopic.portfolio/", // GitHub Pages project repo name
+  },
   mode: process.env.NODE_ENV || "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: [/node_modules/],
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
@@ -25,24 +24,38 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i, 
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: "asset/resource",
-      }
+        generator: {
+          filename: "img/[path][name][ext]", // Preserve subfolders inside src/img
+        },
+        include: path.resolve(__dirname, "src"),
+      },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // Allows importing .js or .jsx without extension
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: "./src/favicon.ico"
+      favicon: "./src/favicon.ico",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/404.html", to: "404.html" }, // Needed for React Router on GitHub Pages
+      ],
     }),
   ],
-  devtool: "cheap-module-source-map", // Fast & useful in development
+  devtool: "cheap-module-source-map",
   devServer: {
     static: path.resolve(__dirname, "docs"),
     port: 3000,
-    open: true, headers: {
-  "Content-Security-Policy": "default-src 'self'; img-src 'self' data: https:;"
-},
-   historyApiFallback: true, // Ensures React Router handles all routes
+    open: true,
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; img-src 'self' data: https:;",
+    },
+    historyApiFallback: true, // React Router SPA fallback
   },
 };
